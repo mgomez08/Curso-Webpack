02a8016e88460e67a95522cde2a786fc5adf7250
Marlon Gómez <marlonjulian15@gmail.com>
2021-03-13 11:54:35 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+import '@styles/style.scss';
+(async function App() {
+  const main = null || document.getElementById('main');
+  main.innerHTML = await Template();
+})();
